8a02d92f8a37e2376f5ca3b8a76cab126a3736e8
Olive <Olive.bien.noir@gmail.com>
2021-08-30 19:25:48 +0200

    Prototypes et signatures des classes

--- Code/Headers/Polynomial.hpp
+++ Code/Headers/Polynomial.hpp
@@ -1,11 +1,62 @@
 #ifndef DEF_POLYNOMIAL_HPP
 #define DEF_POLYNOMIAL_HPP
 
+#include <utility>
+#include <iosfwd>
+#include <string>
+#include <vector>
+
 class Monomial {
 
     public:
 
+    Monomial();
+
+    Monomial(const Monomial&) = default;
+    Monomial(Monomial&&) noexcept = default;
+
+    Monomial(const std::string&);
+    Monomial(int v, unsigned int p = 1);
+
+    ~Monomial() = default;
+
+    bool Null() const;
+
+    int CalculateFor(int);
+
+    friend std::string ToString(Monomial);
+
+    friend Polynomial Factor(const Polynomial&, const Polynomial&);
+    friend std::pair<Polynomial, Polynomial> Develop(const Polynomial&);
+
+    friend Monomial operator+(const Monomial&, const Monomial&);
+    friend Monomial operator-(const Monomial&, const Monomial&);
+    friend Monomial operator*(const Monomial&, const Monomial&);
+    friend Monomial operator/(const Monomial&, const Monomial&);
+
+    Monomial& operator+=(const Monomial&);
+    Monomial& operator-=(const Monomial&);
+    Monomial& operator*=(const Monomial&);
+    Monomial& operator/=(const Monomial&);
+
+    friend bool operator==(const Monomial&, const Monomial&);
+    friend bool operator!=(const Monomial&, const Monomial&);
+
+    friend bool operator<(const Monomial&, const Monomial&);
+    friend bool operator<=(const Monomial&, const Monomial&);
+
+    friend bool operator>(const Monomial&, const Monomial&);
+    friend bool operator>=(const Monomial&, const Monomial&);
+
+    Monomial& operator=(const Monomial&) = default;
+    Monomial& operator=(Monomial&&) noexcept = default;
+
+    friend std::ostream& operator<<(std::ostream&, const Monomial&);
+
     private:
+
+    int value;
+    unsigned int power;
 
 };
 
@@ -13,7 +64,58 @@ class Polynomial {
 
     public:
 
+    Polynomial();
+
+    Polynomial(const Polynomial&) = default;
+    Polynomial(Polynomial&&) noexcept = default;
+
+    Polynomial(Monomial);
+
+    ~Polynomial() = default;
+
+    bool Null() const;
+    bool IsMonomial() const;
+    bool IsBinomial() const;
+    bool IsTrinomial() const;
+
+    int CalculateFor(int);
+    std::size_t Count() const;
+
+    friend std::string ToString(const Polynomial&);
+
+    friend Polynomial Factor(const Polynomial&, const Polynomial&);
+    friend std::pair<Polynomial, Polynomial> Develop(const Polynomial&);
+
+    friend Polynomial operator+(const Polynomial&, const Polynomial&);
+    friend Polynomial operator-(const Polynomial&, const Polynomial&);
+    friend Polynomial operator*(const Polynomial&, const Polynomial&);
+    friend Polynomial operator/(const Polynomial&, const Polynomial&);
+
+    Polynomial& operator+=(const Polynomial&);
+    Polynomial& operator-=(const Polynomial&);
+    Polynomial& operator*=(const Polynomial&);
+    Polynomial& operator/=(const Polynomial&);
+
+    friend bool operator==(const Polynomial&, const Polynomial&);
+    friend bool operator!=(const Polynomial&, const Polynomial&);
+
+    friend bool operator<(const Polynomial&, const Polynomial&);
+    friend bool operator<=(const Polynomial&, const Polynomial&);
+
+    friend bool operator>(const Polynomial&, const Polynomial&);
+    friend bool operator>=(const Polynomial&, const Polynomial&);
+
+    Monomial operator[](std::size_t) const;
+    Monomial& operator[](std::size_t);
+
+    Polynomial& operator=(const Polynomial&) = default;
+    Polynomial& operator=(Polynomial&&) noexcept = default;
+
+    friend std::ostream& operator<<(std::ostream&, const Polynomial&);
+
     private:
+
+    std::vector<Monomial> terms;
 
 };
 
---
+++ README.md
@@ -0,0 +1,3 @@
+# Polynômes
+
+Exercice sur les polynômes de Zeste de savoir : https://zestedesavoir.com/tutoriels/822/la-programmation-en-c-moderne/la-programmation-orientee-objet/t-p-entrons-dans-la-matrice/#des-polyn%C3%B4mes
