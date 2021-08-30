34063880a19b2cc7e410a24612eea569e559866c
Olive <Olive.bien.noir@gmail.com>
2021-08-30 17:57:22 +0200

    Initialisation

---
+++ Code/Headers/Polynomial.hpp
@@ -0,0 +1,20 @@
+#ifndef DEF_POLYNOMIAL_HPP
+#define DEF_POLYNOMIAL_HPP
+
+class Monomial {
+
+    public:
+
+    private:
+
+};
+
+class Polynomial {
+
+    public:
+
+    private:
+
+};
+
+#endif // DEF_POLYNOMIAL_HPP
---
+++ Code/Sources/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+#include "../Headers/Polynomial.hpp"
+
+int main() {
+
+    return 0;
+
+}
+
+// Command : g++ -m32 "./Code/sources/*.cpp" -o "./bin/Polynômes" -std=c++17 -Wall -Wextra -pedantic-errors -O3
